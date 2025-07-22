7c0da827e565664b3fdced3ea3878a418ab2c88f
MHDKIEMDE <mkiemde00@gmail.com>
2025-07-22 17:33:17 +0000

    page Welcome

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+
+Route::get('/about-us', function () {
+    return view('about-us');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/cart', function () {
+    return view('cart');
+})->name('cart');
